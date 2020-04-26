d68faa8e72453fb4c7dca5f444aa05d2e637ae3d
shijiaw <shijiaw@sfu.ca>
2020-04-26 21:26:40 +0800

    Add files via upload

---
+++ src/GSM/gsm.java
@@ -0,0 +1,38 @@
+package GSM;
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import fig.basic.IOUtils;
+import goblin.Taxon;
+import nuts.io.CSV;
+import nuts.io.IO;
+import pty.RootedTree;
+import pty.UnrootedTree;
+
+public class gsm {
+	public static void main(String[] args) {
+				
+		String inputTree = "/Users/oudomame/EGSM/Example/tree1.newick";
+		String OutputFileName = "/Users/oudomame/EGSM/Example/K.csv";
+		PrintWriter KOut = IOUtils.openOutEasy(new File(OutputFileName));
+		//PrintWriter KOut = IOUtils.openOutEasy(new File("/Users/oudomame/EGSM/Example/", OutputFileName));
+		
+		File dataFile = new File(inputTree);
+		UnrootedTree goldut = UnrootedTree.fromRooted(RootedTree.Util.fromNewickString(IO.f2s(dataFile)));
+		List<Taxon> leaves = goldut.leaves();
+		String st = dataFile.getName();
+		double[][] K = phylogeny.tree2K(goldut, leaves);
+		
+		for(int i = 0; i < leaves.size(); i++) {
+			List<Double> Kind = new ArrayList<>();
+			for(int ind = 0; ind < leaves.size(); ind++) {
+				Kind.add(K[i][ind]);
+			}
+			KOut.println(CSV.body(Kind));
+		}		
+		
+		KOut.close();
+	}
+}
